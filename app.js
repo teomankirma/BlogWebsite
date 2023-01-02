9d6f553e22af69a7619143f9a23284ad66e3eed4
teomankirma <83593653+teomankirma@users.noreply.github.com>
2023-01-02 13:51:41 +0300

    feat: challenge 10 solution

--- app.js
+++ app.js
@@ -33,7 +33,10 @@ app.get("/compose", function (req, res) {
 });
 
 app.post("/compose", function (req, res) {
-  console.log(req.body.postTitle);
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody,
+  };
 });
 app.listen(3000, function () {
   console.log("Server started on port 3000");
